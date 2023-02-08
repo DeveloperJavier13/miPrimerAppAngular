10589de6f7dbcc7a800af74f32f0ad7ad60be48d
DeveloperJavier13 <73495101+DeveloperJavier13@users.noreply.github.com>
2023-02-08 12:15:28 -0600

    Initial commit

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+})
+export class EmpleadoComponent {
+  nombre="Juan";
+  apellido="Díaz";
+  edad=18;
+  empresa="Video 11";
+  /*cambiaEmpresa(event:Event){
+    this.empresa=(<HTMLInputElement>event.target).value;
+  }*/
+  /*getEdad(){
+    return this.edad;
+  }*/
+
+  habilitacionCuadro=false;
+
+  usuRegistrado=false;
+
+  textoRegistro="No hay registros";
+  getRegistroUsuario(){
+    this.usuRegistrado=false;
+  }
+  setUsuarioRegistrado(event:Event){
+    //alert("El usuario esta registrado");
+    //this.textoRegistro="Ya hay registro";
+    //alert(event.target);
+    if((<HTMLInputElement>event.target).value=="si"){
+      this.textoRegistro="Ya hay registro";
+    }else{
+      this.textoRegistro="No hay registro";
+    }
+  }
+
+
+  
+
+}
